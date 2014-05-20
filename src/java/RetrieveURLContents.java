e83c46774e36ba9443ede1e6f50c1fc28a4ff6d0
MSaifAsif <saifasifmirza@gmail.com>
2014-05-20 13:05:06 +0500

    Fixed compile time issues

--- src/java/RetrieveURLContents.java
+++ src/java/RetrieveURLContents.java
@@ -10,10 +10,10 @@ public class RetrieveURLContents {
 	public static void main(String[] args) throws IOException {
 		URL url = new URL("http://www.google.com");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-	    	conn.setRequestProperty("Cookie", "");
-		conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-		conn.addRequestProperty("User-Agent", "");
-		conn.addRequestProperty("Referer", "");
+	    	connection.setRequestProperty("Cookie", "");
+		connection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+		connection.addRequestProperty("User-Agent", "");
+		connection.addRequestProperty("Referer", "");
 		System.out.println(connection.getResponseCode());
 		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 		while(br.read() != -1){

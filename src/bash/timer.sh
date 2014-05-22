0eff76d0100244f69e1baadb633f1a0f795c0b97
MSaifAsif <saifasifmirza@gmail.com>
2014-05-22 10:05:11 +0500

    Docs added

--- src/bash/timer.sh
+++ src/bash/timer.sh
@@ -1,5 +1,12 @@
+#
+# Author: Saif Asif
+# Version : 1.2
+#
+# Utility to calculate the time required to execite a certain command
+#
+
 date1=$(date +"%s")
-# Do your work here
+# Your command here
 date2=$(date +"%s")
 diff=$(($date2-$date1))
 echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
